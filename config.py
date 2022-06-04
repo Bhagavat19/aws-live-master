77eaf0876707b209bc62aa6d789dd2d734992342
Bhagavat19 <bhagavatbhise56@gmail.com>
2022-06-04 16:36:14 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cdwntx6dkmws.ap-south-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "bhagavat"
+customdb = "employee"
+custombucket = "adwemployee"
+customregion = "ap-south-1"
